7da67a98e8bde09bbe3cd46143d36f21b811cd4b
3lianderson <eliandersonscosta@gmail.com>
2025-09-04 14:07:18 -0300

    bucket and html site

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variable "profile" {
-  type    = string
-  default = "default"
+variable "region_name" {
+  description = "The AWS region to create resources in."
+  type        = string
 }
 
-variable "region-master" {
-  type    = string
-  default = "us-east-1"
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
 }
